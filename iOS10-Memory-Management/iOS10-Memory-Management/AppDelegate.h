911cd0439a2d0e4d72e5dc29859aa1611744cd3f
Paul Solt <PaulSolt@gmail.com>
2019-12-18 12:30:47 -0500

    Added starter project

---
+++ iOS10-Memory-Management/iOS10-Memory-Management/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS10-Memory-Management
+//
+//  Created by Paul Solt on 12/18/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOS10-Memory-Management/iOS10-Memory-Management/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS10-Memory-Management
+//
+//  Created by Paul Solt on 12/18/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
